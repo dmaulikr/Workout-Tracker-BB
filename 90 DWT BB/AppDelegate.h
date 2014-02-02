d9ce4252b3b8e8de3b053fc076dc7fdd774cd7d3
Jared Grant <jaredgrant@Jareds-iMac.local>
2014-02-01 18:52:25 -0700

    Initial commit.

---
+++ 90 DWT BB/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  90 DWT BB
+//
+//  Created by Jared Grant on 2/1/14.
+//  Copyright (c) 2014 Jared Grant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ 90 DWT BB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  90 DWT BB
+//
+//  Created by Jared Grant on 2/1/14.
+//  Copyright (c) 2014 Jared Grant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
