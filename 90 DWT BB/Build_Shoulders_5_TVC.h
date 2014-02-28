a93033efaa89f9889196920297777eda8321e917
Jared Grant <jaredgrant@Jareds-iMac.local>
2014-02-27 20:03:45 -0700

    Finished Build: Shoulders TVC’s.

---
+++ 90 DWT BB/Build_Shoulders_5_TVC.h
@@ -0,0 +1,21 @@
+//
+//  Build_Shoulders_5_TVC.h
+//  90 DWT BB
+//
+//  Created by Jared Grant on 2/27/14.
+//  Copyright (c) 2014 Jared Grant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <iAd/iAd.h>
+#import "ExerciseCell.h"
+
+@interface Build_Shoulders_5_TVC : UITableViewController
+
+@property (strong, nonatomic) NSArray *Titles;
+@property (strong, nonatomic) NSArray *Reps;
+
+@property (strong, nonatomic) NSMutableArray *CellArray;
+
+- (IBAction)submitEntries:(id)sender;
+@end
